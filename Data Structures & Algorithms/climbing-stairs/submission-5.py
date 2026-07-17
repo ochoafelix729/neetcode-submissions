d5c17edeeee4490cd3de279e02ba3a95c0207bda
Felix Ochoa <109564794+ochoafelix729@users.noreply.github.com>
2026-07-17 14:29:22 -0500

    Add: climbing-stairs - submission-5

---
+++ Data Structures & Algorithms/climbing-stairs/submission-5.py
@@ -0,0 +1,45 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+
+        # bottom-up DP approach
+        # O(n) time, O(1) space
+
+        if n <= 2:
+            return n
+        
+        prev, cur = 1, 2
+        for _ in range(3, n+1):
+            cur, prev = prev + cur, cur
+        return cur
+
+        # bottom-up DP approach
+        # O(n) time, O(n) space
+        
+        # if n <= 2:
+        #     return n
+
+        # dp = [0] * (n+1)
+        # dp[1], dp[2] = 1, 2
+        # for i in range(3, n+1):
+        #     dp[i] = dp[i-1] + dp[i-2]
+        # return dp[n]
+
+        
+        # top-down DP approach
+        # O(n) time, O(n) space
+        
+        # cache = [-1] * (n+1)
+
+        # def dfs(i):
+        #     if i >= n:
+        #         return 1
+            
+        #     if cache[i] != -1:
+        #         return cache[i]
+
+        #     step = dfs(i+1)
+        #     leap = dfs(i+2)
+        #     cache[i] = step + leap
+        #     return cache[i]
+        
+        # return dfs(1)
