6b7c9b44e0961042fac21ebbfe0d10f64dbbfd32
Felix Ochoa <109564794+ochoafelix729@users.noreply.github.com>
2026-07-23 14:11:05 -0500

    Add: path-with-minimum-effort - submission-1

---
+++ Data Structures & Algorithms/path-with-minimum-effort/submission-1.py
@@ -0,0 +1,44 @@
+class Solution:
+    def minimumEffortPath(self, heights: List[List[int]]) -> int:
+        # djikstra's
+
+        ROWS = len(heights)
+        COLS = len(heights[0])
+
+        minCost = [[float('inf') for _ in range(COLS)] for _ in range(ROWS)]
+ 
+        for r in range(ROWS):
+            for c in range(COLS):
+                minCost[r][c] = float('inf')
+        
+        minheap = [(0, 0, 0)] # effort, row, column
+        visited = set()
+        maxDiff = 0
+        totalDiff = 0
+        directions = [(1,0), (-1,0), (0,1), (0,-1)]
+
+        while minheap:
+            e1, r, c = heapq.heappop(minheap)
+            if e1 > minCost[r][c]:
+                continue
+
+            print(e1, r, c)
+            totalDiff += e1
+            maxDiff = max(maxDiff, e1)
+
+            if r == ROWS - 1 and c == COLS - 1:
+                break
+            visited.add((r,c))
+            
+            for dr, dc in directions:
+                nr, nc = r + dr, c + dc
+                if (nr,nc) in visited or nr not in range(ROWS) or nc not in range(COLS):
+                    continue
+                diff = abs(heights[nr][nc] - heights[r][c])
+                if  diff < minCost[nr][nc]:
+                    #print('hi')
+                    heapq.heappush(minheap, (diff, nr, nc))
+                    minCost[nr][nc] = diff
+ 
+        #print(totalDiff)
+        return maxDiff
