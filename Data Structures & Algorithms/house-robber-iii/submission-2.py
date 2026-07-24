a719a2cd2c52581c819613938a74135711d6cbf8
Felix Ochoa <109564794+ochoafelix729@users.noreply.github.com>
2026-07-24 16:38:52 -0500

    Add: house-robber-iii - submission-2

---
+++ Data Structures & Algorithms/house-robber-iii/submission-2.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rob(self, root: Optional[TreeNode]) -> int:
+        def dfs(node, blocked, cache):
+            if node is None:
+                return 0
+            
+            if (node, blocked) in cache:
+                return cache[(node, blocked)]
+
+            if blocked:
+                cache[(node, blocked)] = dfs(node.left, False, cache) + dfs(node.right, False, cache)
+            
+            else:
+                cache[(node, blocked)] = max(
+                    node.val + dfs(node.left, True, cache) + dfs(node.right, True, cache),
+                    dfs(node.left, False, cache) + dfs(node.right,False, cache)
+                )
+
+            return cache[(node, blocked)]
+
+        return dfs(root, False, defaultdict(int))
